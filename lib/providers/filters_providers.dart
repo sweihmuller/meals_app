35fda5d48352be22aeed7d43d3bd87ac9515ad5b
Santi <sweihmuller01@gmail.com>
2025-07-27 18:44:22 -0300

    Connecting multiple Providers with each others

--- lib/providers/filters_providers.dart
+++ lib/providers/filters_providers.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meals_app/screens/filters.dart';
+import 'package:meals_app/providers/meals_providers.dart';
 
 enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
 
@@ -25,3 +25,23 @@ final filtersProviders =
     StateNotifierProvider<FiltersNotifiers, Map<Filter, bool>>(
       (ref) => FiltersNotifiers(),
     );
+
+final filterMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProviders);
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
